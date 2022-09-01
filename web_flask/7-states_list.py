2dc13fcf586f62ea50cda1e76f24cae58122080c
agusfl <agusflom@hotmail.com>
2022-09-01 16:04:59 +0000

    try

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -43,9 +43,8 @@ def states_list():
     """
     Import data from storage
     """
-    state_objs = [s for s in storage.all("State").values()]
-    return render_template('7-states_list.html',
-                           state_objs=state_objs)
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)
 
 
 if __name__ == "__main__":
