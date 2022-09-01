eca3855b91ecd011c2c800a14ac38feb24cdec52
agusfl <agusflom@hotmail.com>
2022-09-01 17:47:46 +0000

    fix printing

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -43,9 +43,7 @@ def states_list():
     Import data from storage
     """
     states = storage.all("State").values()
-    cities = storage.all("City").values()
-    return render_template("8-cities_by_states.html", states=states,
-                           cities=cities)
+    return render_template("8-cities_by_states.html", states=states)
 
 
 if __name__ == "__main__":
